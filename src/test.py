9b3446597f9a140d6bc23fb1955e1ee94dfa1d2a
BW <boris@victorov.com>
2026-06-26 17:08:33 +0300

    Added yaml config

--- src/test.py
+++ src/test.py
@@ -4,23 +4,21 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import IntegerType
 from pyspark.ml.clustering import KMeans
 from pyspark.ml.feature import VectorAssembler
-# from pyspark.pandas import read_parquet
+import yaml
+
+with open('config/spark_config.yaml') as f:
+    cfg = yaml.safe_load(f)
+
 os.environ["PYARROW_IGNORE_TIMEZONE"] = "1" 
-spark = SparkSession.builder.appName("MyApp").master("local[*]") \
-     .config("spark.driver.memory", "8g") \
-    .config("spark.executor.memory", "2g") \
-    .config("spark.driver.maxResultSize", "2g") \
-    .config("spark.sql.adaptive.enabled", "true") \
-    .config("spark.sql.adaptive.coalescePartitions.enabled", "true") \
-    .config("spark.sql.shuffle.partitions", "8") \
-    .config("spark.default.parallelism", "8") \
-    .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer") \
-    .config("spark.sql.execution.arrow.pyspark.enabled", "true") \
-    .config("spark.sql.files.maxPartitionBytes", "64m") \
-    .config("spark.sql.files.openCostInBytes", "4194304") \
-    .config("spark.hadoop.dfs.client.use.datanode.hostname", "true") \
-    .config("spark.hadoop.dfs.datanode.use.datanode.hostname", "true") \
-    .getOrCreate()
+builder = SparkSession.builder.appName(cfg['spark']['app_name']).master(cfg['spark']['master'])
+    
+
+for key, value in cfg['spark']['configs'].items():
+    builder = builder.config(key, value)
+
+
+spark = builder.getOrCreate()
+
 df = spark.read.parquet('hdfs://localhost:8020/test/food.parquet')
 
 
